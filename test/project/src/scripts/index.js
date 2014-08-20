dbd9c5dd011aa9a00f33d4ef8710b7d2b7db1ea8
dong.gao <dong.gao@qunar.com>
2014-08-20 16:37:35 +0800

    test case

--- test/project/src/scripts/index.js
+++ test/project/src/scripts/index.js
@@ -2,12 +2,12 @@ require(['./index/a.js',
 	'./index/b.js'
 ],function(){
 
-	(typeof require=='function')&&require([
+	require([
 		'./index/a.js',
 		'./index/b.js',
 		'./require/index.js'
 	],function(){
-		var a="";
-		console.log('load index');
+		var a="foo bar";
+		console.log(a);
 	});
 });
